52763dcf47f98b8c935b12e23a908cf1fec158e9
Walker <lazyrf.tw@gmail.com>
2023-03-22 21:13:42 +0800

    Implementing error and success messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,7 +28,7 @@ const controlRecipes = async function() {
         // 2) Rendering recipe
         recipeView.render(model.state.recipe);
     } catch (err) {
-        config.log(err);
+        recipeView.renderError();
     }
 };
 
