4837182548a6c4281b2f61b879cc5a125430e6f1
Walker <lazyrf.tw@gmail.com>
2023-03-22 20:23:36 +0800

    helpers.js and config.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,14 +8,6 @@ import 'regenerator-runtime/runtime'; // polyfill async/await
 
 // console.log(icons); // http://localhost:1234/icons.dfd7a6db.svg?1679457050731
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
