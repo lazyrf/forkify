70dcc6bd83cf1cf1623f7909f7e253e6d7ebdd36
Walker <lazyrf.tw@gmail.com>
2023-03-23 19:33:32 +0800

    Implementing search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,7 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
+import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 
 // import icons from '../img/icons.svg'; // Parcel 1
 // import icons from 'url:../img/icons.svg'; // Parcel 2
@@ -11,7 +13,6 @@ import 'regenerator-runtime/runtime'; // polyfill async/await
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-
 
 const controlRecipes = async function() {
     try {
@@ -32,7 +33,28 @@ const controlRecipes = async function() {
     }
 };
 
+const controlSearchResults = async function() {
+    try {
+        resultsView.renderSpinner();
+
+        // 1) Get search query
+        const query = searchView.getQuery();
+        if (!query) {
+            return ;
+        }
+
+        // 2) Load search results
+        await model.loadSearchResults(query);
+
+        // 3) Render result
+        resultsView.render(model.state.search.results);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
 const init = function() {
     recipeView.addHandlerRender(controlRecipes);
+    searchView.addHandlerRender(controlSearchResults);
 };
 init();
