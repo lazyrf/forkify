138ab9dad9c5d61fcf02ad05e11d4d836f8b29bd
Walker <lazyrf.tw@gmail.com>
2023-03-24 14:58:52 +0800

    Updating recipe servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -65,8 +65,17 @@ const controlPagination = function(page) {
     paginationView.render(model.state.search);
 };
 
+const controlServings = function(newServings) {
+    // Update the recipe servings (in state)
+    model.updateServings(newServings);
+
+    // Update the view
+    recipeView.render(model.state.recipe);
+};
+
 const init = function() {
     recipeView.addHandlerRender(controlRecipes);
+    recipeView.addHandlerUpdateServings(controlServings);
     searchView.addHandlerRender(controlSearchResults);
     paginationView.addHandlerRender(controlPagination);
 };
