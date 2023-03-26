574205a416ebba2dfe853bbfd69dae63f43a3d4d
Walker <lazyrf.tw@gmail.com>
2023-03-26 17:30:26 +0800

    Uploading a new recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,11 @@
 import * as model from "./model.js";
+import { MODAL_CLOSE_SEC } from "./config.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
 import paginationView from "./views/paginationView.js";
 import bookmarksView from "./views/bookmarksView.js";
+import addRecipeView from "./views/addRecipeView.js";
 
 // import icons from '../img/icons.svg'; // Parcel 1
 // import icons from 'url:../img/icons.svg'; // Parcel 2
@@ -98,6 +100,37 @@ const controlBookmarks = function() {
     bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = async function(newRecipe) {
+    try {
+        // Show loading spinner
+        addRecipeView.renderSpinner();
+
+        // Upload the new recipe data
+        await model.uploadRecipe(newRecipe);
+        console.log(model.state.recipe);
+
+        // Render recipe
+        recipeView.render(model.state.recipe);
+
+        // Success message
+        addRecipeView.renderMessage();
+
+        // Render bookmark view
+        bookmarksView.render(model.state.bookmarks);
+
+        // Change ID in URL
+        window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+        // Close form window
+        setTimeout(function() {
+            addRecipeView.toggleWindow();
+        }, MODAL_CLOSE_SEC * 1000);
+    } catch(err) {
+        console.error(err);
+        addRecipeView.renderError(err.message);
+    }
+};
+
 const init = function() {
     bookmarksView.addHandlerRender(controlBookmarks);
     recipeView.addHandlerRender(controlRecipes);
@@ -105,5 +138,6 @@ const init = function() {
     recipeView.addHandlerAddBookmark(controlAddBookmark);
     searchView.addHandlerRender(controlSearchResults);
     paginationView.addHandlerRender(controlPagination);
+    addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
