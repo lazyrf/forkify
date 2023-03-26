fd87ae24a04015e0b5ce17f66f9e81e3c330af4c
Walker <lazyrf.tw@gmail.com>
2023-03-26 13:47:50 +0800

    Implementing Bookmarks

--- src/js/model.js
+++ src/js/model.js
@@ -9,7 +9,8 @@ export const state = {
         results: [],
         page: 1,
         resultsPerPage: RES_PER_PAGE,
-    }
+    },
+    bookmarks: []
 };
 
 export const loadRecipe = async function(id) {
@@ -27,6 +28,12 @@ export const loadRecipe = async function(id) {
             cookingTime: recipe.cooking_time,
             ingredients: recipe.ingredients
         };
+
+        if (state.bookmarks.some(bookmark => bookmark.id === id)) {
+            state.recipe.bookmarked = true;
+        } else {
+            state.recipe.bookmarked = false;
+        }
         console.log(state.recipe);
     } catch (err) {
         throw err;
@@ -69,3 +76,24 @@ export const updateServings = function(newServings) {
 
     state.recipe.servings = newServings;
 }
+
+export const addBookmark = function(recipe) {
+    // Add bookmark
+    state.bookmarks.push(recipe);
+
+    // Mark current recipe as bookmarked
+    if (recipe.id === state.recipe.id) {
+        state.recipe.bookmarked = true;
+    }
+};
+
+export const deleteBookmark = function(id) {
+    // Delete bookmark
+    const index = state.bookmarks.findIndex(el => el.id === id);
+    state.bookmarks.splice(index, 1);
+
+    // Mark current recipe as NOT bookmarked
+    if (id === state.recipe.id) {
+        state.recipe.bookmarked = false;
+    }
+};
