2a236b1cab5eb4105510b6c392cba022777258c7
Walker <lazyrf.tw@gmail.com>
2023-03-26 15:12:48 +0800

    Create a parent class - PreviewView

---
+++ src/js/views/previewView.js
@@ -0,0 +1,26 @@
+import View from "./view.js";
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+export default class PreviewView extends View {
+    _generateMarkup() {
+        return this._data.map(this._generateMarkupPreivew).join('');
+    }
+
+    _generateMarkupPreivew(result) {
+        const id = window.location.hash.slice(1);
+
+        return `
+         <li class="preview">
+            <a class="preview__link ${result.id === id ? 'preview__link--active' : ''}" href="#${result.id}">
+              <figure class="preview__fig">
+                <img src="${result.image}" alt="Test" />
+              </figure>
+              <div class="preview__data">
+                <h4 class="preview__title">${result.title}</h4>
+                <p class="preview__publisher">${result.publisher}</p>
+              </div>
+            </a>
+          </li>
+        `;
+    }
+}
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,32 +1,10 @@
-import View from "./view.js";
+import PreviewView from "./previewView.js";
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 
-class ResultsView extends View {
+class ResultsView extends PreviewView {
     _parentEl = document.querySelector(".results");
     _errorMessage = 'No recipes found for your query! Please try again :)';
     _message = "";
-
-    _generateMarkup() {
-        return this._data.map(this._generateMarkupPreivew).join('');
-    }
-
-    _generateMarkupPreivew(result) {
-        const id = window.location.hash.slice(1);
-
-        return `
-         <li class="preview">
-            <a class="preview__link ${result.id === id ? 'preview__link--active' : ''}" href="#${result.id}">
-              <figure class="preview__fig">
-                <img src="${result.image}" alt="Test" />
-              </figure>
-              <div class="preview__data">
-                <h4 class="preview__title">${result.title}</h4>
-                <p class="preview__publisher">${result.publisher}</p>
-              </div>
-            </a>
-          </li>
-        `;
-    }
 }
 
 export default new ResultsView();
