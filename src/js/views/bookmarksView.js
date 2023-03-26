a0d18bba6f6832c39af8665787616b5ecc0ab76c
Walker <lazyrf.tw@gmail.com>
2023-03-26 15:38:13 +0800

    Storing Bookmarks with localStorage

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -5,6 +5,10 @@ class BookmarksView extends PreviewView {
     _parentEl = document.querySelector(".bookmarks__list");
     _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it :)';
     _message = "";
+
+    addHandlerRender(handler) {
+        window.addEventListener('load', handler);
+    }
 }
 
 export default new BookmarksView();
